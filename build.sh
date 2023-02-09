8d292a02b8db75d934b9415e864ba05f5fe7fc5f
Jeromekene <mckenzieelegeonye@gmail.com>
2023-02-10 00:53:34 +0100

    Add build file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start the node app with nodemon
+nodemon server.js | bunyan -o short
+
+# Wait for the node app to start
+sleep 5
+
+# Run the sequelize migrations
+./node_modules/.bin/sequelize db:migrate
+
+# Keep the script running so that the node app keeps running
+while true; do
+  sleep 1
+done
