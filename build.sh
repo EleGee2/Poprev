e31babcea8d35b415d78f26d1de4ee56ecb9dcf2
Jeromekene <mckenzieelegeonye@gmail.com>
2023-02-10 08:38:14 +0100

    Update build file

--- build.sh
+++ build.sh
@@ -9,10 +9,6 @@ nodemon server.js | bunyan -o short
 # Wait for the node app to start
 sleep 5
 
-# Run the sequelize migrations
-./node_modules/.bin/sequelize db:migrate
+# Run the sequelize migration
+npx sequelize db:migrate
 
-# Keep the script running so that the node app keeps running
-while true; do
-  sleep 1
-done
